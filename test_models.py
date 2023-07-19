b5c29166e1b11b89f989a33f192611bc3e195a63
Krishnan Raghavan <krm9c@outlook.com>
2023-07-19 22:43:32 +0000

    The final code is ready to go, I am oing to start plotting.

--- test_models.py
+++ test_models.py
@@ -11,7 +11,7 @@ import matplotlib.cm as cm
 import itertools
 sns.color_palette("bright")
 from odevae import *
-
+import copy
 
 def to_np(x):
     return x.detach().cpu().numpy()
@@ -48,18 +48,19 @@ def conduct_experiment_latent(X, step_model_optimizer_loss, save_path, device='c
     ETr = E[permutation]
     h_omegaT = h_omega[permutation]
     # Train Neural ODE
-    prev_epoch, ode_trained, optimizer, prev_loss = step_model_optimizer_loss
+    prev_epoch, ode_trained, optimizer_adam, prev_loss = step_model_optimizer_loss
     print("Starting training from epoch ", prev_epoch, flush=True)
-    for i in range(prev_epoch, epochs):
+    for i in range(prev_epoch, epochs ):
         obs_, ts_, ho_ = create_batch_latent(ETr, h_omegaT, N_Max)
         input_d = torch.cat( [obs_, ho_], axis=2).to(device)
         x_p, z, z_mean, z_log_var = ode_trained(input_d, ts_.to(device))
         kl_loss = -0.5 * torch.sum(1 + z_log_var - z_mean**2 - torch.exp(z_log_var), -1)
         error_loss = 0.5 * ((input_d-x_p)**2).sum(-1).sum(0) / noise_std**2
+        
         loss = torch.mean(error_loss+ kl_loss)
-        optimizer.zero_grad()
+        optimizer_adam.zero_grad()
         loss.backward(retain_graph=True)
-        optimizer.step()
+        optimizer_adam.step()
         if i % print_iter == 0:
             print("(Print) Epoch:", i, "Total_Loss: "+str(loss.item()) + " with error", str(torch.mean(error_loss).item())+\
                   " KL divergence " + str(torch.mean(kl_loss).item()), flush=True)
@@ -69,7 +70,7 @@ def conduct_experiment_latent(X, step_model_optimizer_loss, save_path, device='c
             torch.save({
                 'step': i,
                 'model_state_dict': ode_trained.state_dict(),
-                'optimizer_state_dict': optimizer.state_dict(),
+                'optimizer_state_dict': optimizer_adam.state_dict(),
                 'loss': loss
                 },
                 save_path)
@@ -98,6 +99,88 @@ def conduct_experiment_latent(X, step_model_optimizer_loss, save_path, device='c
                 + str(np.round(torch.mean(error_loss).item(), 2))+" \n KL divergence: " + str(np.round(torch.mean(kl_loss).item(), 2)))
             plt.savefig('Figures/training/reconstruction_'+str(i)+'.png', dpi= 300,  bbox_inches="tight")
             plt.close()
+
+
+    optimizer_BFGS = torch.optim.LBFGS(ode_trained.parameters(),                    
+                                    history_size=20, 
+                                    max_iter=2)
+    def closure():
+        x_p, _, _, _= ode_trained(input_d, ts_.to(device))
+        error_loss = ((input_d-x_p)**2).sum(-1).sum(0) / noise_std**2
+        loss = torch.mean(error_loss)
+        optimizer_BFGS.zero_grad()
+        loss.backward()
+        return loss
+    obs_, ts_, ho_ = create_batch_latent(ETr, h_omegaT, N_Max)
+    input_d = torch.cat( [obs_, ho_], axis=2).to(device)
+    copy_net = copy.deepcopy(ode_trained)
+    for i in range( 500):
+        optimizer_BFGS.step(closure)
+        if torch.isnan(closure()):
+            print("The loss is nan, I am copying things")
+            ode_trained = copy.deepcopy(copy_net)
+            optimizer_BFGS = torch.optim.LBFGS(ode_trained.parameters(), lr=0.1,
+                    history_size=10, 
+                    max_iter=2)
+        else:
+            copy_net = copy.deepcopy(ode_trained)
+        
+        if i % 10 == 0:
+            obs_, ts_, ho_ = create_batch_latent(ETr, h_omegaT, N_Max)
+            input_d = torch.cat( [obs_, ho_], axis=2).to(device)
+            x_p, z, z_mean, z_log_var = ode_trained(input_d, ts_.to(device))
+            kl_loss = -0.5 * torch.sum(1 + z_log_var - z_mean**2 - torch.exp(z_log_var), -1)
+            error_loss = 0.5 * ((input_d-x_p)**2).sum(-1).sum(0) / noise_std**2
+            loss = torch.mean(error_loss+ kl_loss)
+            print("(Print) Epoch:", i, "Total_Loss: "+str(loss.item()) + " with error", str(torch.mean(error_loss).item())+\
+                " KL divergence " + str(torch.mean(kl_loss).item()), flush=True)
+            
+
+        if i % 10 == 0 or i == 100:
+            obs_, ts_, ho_ = create_batch_latent(ETr, h_omegaT, N_Max)
+            input_d = torch.cat( [obs_, ho_], axis=2).to(device)
+            x_p, z, z_mean, z_log_var = ode_trained(input_d, ts_.to(device))
+            kl_loss = -0.5 * torch.sum(1 + z_log_var - z_mean**2 - torch.exp(z_log_var), -1)
+            error_loss = 0.5 * ((input_d-x_p)**2).sum(-1).sum(0) / noise_std**2
+            loss = torch.mean(error_loss+ kl_loss)
+
+            print("(Save) Epoch:", i, "Total_Loss: "+str(loss.item()) + " with error", str(torch.mean(error_loss).item())+\
+                " KL divergence " + str(torch.mean(kl_loss).item()), flush=True)
+            torch.save({
+                'step': epochs+i,
+                'model_state_dict': ode_trained.state_dict(),
+                'optimizer_state_dict': optimizer_adam.state_dict(),
+                'loss': loss
+                },
+                save_path)
+            obs_, ts_, ho_ = create_batch_latent_order(E, h_omega, N_Max)
+            input_d = torch.cat([obs_, ho_], axis=2).to(device)
+            samp_trajs_p = to_np(ode_trained.generate_with_seed(input_d, ts_.to(device)))
+            #print(samp_trajs_p.shape)
+
+            plt.figure()
+            fig, axes = plt.subplots(nrows=3, ncols=6, facecolor='white', figsize=(9, 9),\
+                    gridspec_kw={'wspace': 0.5, 'hspace': 0.5}, dpi=400)
+            axes = axes.flatten()
+            for j, ax in enumerate(axes):
+                ax.plot(18*to_np(ts_[:, j, 0]), to_np(input_d[:, j, 0]), label='real', linewidth = 1)
+                ax.scatter(18*ts_[:, j, 0], samp_trajs_p[:, j, 0], 3,
+                        label="predicted" , marker='*',
+                        c=samp_trajs_p[:, j, 0], cmap=cm.plasma)
+                ax.grid("True")
+                ax.set_xlabel('NMax, \n h$\\Omega$ ='\
+                    +str( np.round(ho_[0,j,0].item()*50, 2 )), fontsize = 10)
+                if j == 5:
+                    ax.legend(bbox_to_anchor=(1.05, 1.0), loc='upper left')
+                if j == 0 or j==6 or j ==12:
+                    ax.set_ylabel('Ground state Energy', fontsize = 10)
+            plt.text(20, 75, "\n Total loss: "+str(np.round(loss.item(), 2)) + "\n with error: "\
+                + str(np.round(torch.mean(error_loss).item(), 2))+" \n KL divergence: " + str(np.round(torch.mean(kl_loss).item(), 2)))
+            plt.savefig('Figures/training/reconstruction_'+str(i)+'.png', dpi= 300,  bbox_inches="tight")
+            plt.close()
+
+
+
 
 
 ########################################################################
@@ -178,7 +261,7 @@ def plot_model_averaged_(n_models, X):
         if j % num_cols == 0:
             ax.set_ylabel('Ground state Energy', fontsize=10)
             ax.set_title("Ground State Energy averaged  w.r.t. models")
-        ax.set_ylim(-35, -15)
+        ax.set_ylim(-32.5, -30)
         ax.set_xlim(left=4)
         ax.get_legend_handles_labels()
     handles, labels = plt.gca().get_legend_handles_labels()
@@ -193,13 +276,14 @@ def load_checkpoint(path, device='cpu'):
     loss = 0
     model = ODEVAE(2, 128, 6)
     model.to(device)
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+    optimizer=torch.optim.Adam(model.parameters(), lr=1e-3)
     if os.path.exists(path):
         checkpoint = torch.load(path)
         step = checkpoint['step']
         loss = checkpoint['loss']
         model.load_state_dict(checkpoint['model_state_dict'])
         optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+
     return step, model, optimizer, loss
 
 
@@ -208,7 +292,7 @@ def train_model(stuff):
     path = 'models/Trained_ode_'+str(i)
     X = np.load('data/processed_extrapolation.npy', allow_pickle=True)
     print(F'Launch training of model {i} on process {mp.current_process().pid}', flush=True)
-    conduct_experiment_latent(X, model, path, device=device, epochs=30000, save_iter=100, print_iter=10)
+    conduct_experiment_latent(X, model, path, device=device, epochs=30000, save_iter=10000, print_iter=5000)
 
 
 ########################################################################
@@ -218,7 +302,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
                         prog='test_models.py',
                         description='Tests the ODEVAE model for the No-Core Shell Model (NCSM)')
-
     parser.add_argument('-t', '--train', default=False, action='store_true', help='do training')
     parser.add_argument('-p', '--plot', default=True, action='store_true', help='generate plots with test data')
 
@@ -226,15 +309,19 @@ if __name__ == '__main__':
 
     mp.set_start_method('spawn')
     use_cuda = torch.cuda.is_available()
-    use_cuda = True
+    print(torch.cuda.device_count())
+    
+    # use_cuda = True
     if use_cuda:
         devices = [torch.device('cuda:%d' % i) for i in range(torch.cuda.device_count())]
         num_devices = len(devices)
         num_processes = num_devices
     else:
-        num_processes = 1
+        num_processes = 8
         devices = [torch.device('cpu')] * num_processes
         num_devices = len(devices)
+
+
     print(F'use_cuda = {use_cuda}, num_devices = {num_devices}, num_processes = {num_processes}', flush=True)
 
 
