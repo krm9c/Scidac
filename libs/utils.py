56ea7a4c69ec191914fe4bffc200ec3923c81138
Krishnan Raghavan <krm9c@outlook.com>
2024-06-21 16:58:38 -0500

    added something with respect to the libraries

---
+++ libs/utils.py
@@ -0,0 +1,69 @@
+import jax
+from functools import partial
+import matplotlib.pyplot as plt
+import numpy as np 
+
+
+
+@partial(jax.jit, static_argnums=(2))
+def sp_matmul(A, B, shape):
+    """
+    Arguments:
+        A: (N, M) sparse matrix represented as a tuple (indexes, values)
+        B: (M,K) dense matrix
+        shape: value of N
+    Returns:
+        (N, K) dense matrix
+    """
+    assert B.ndim == 2
+    indexes, values = A
+    rows, cols = indexes
+    in_ = B.take(cols, axis=0)
+    prod = in_*values[:, None]
+    res = jax.ops.segment_sum(prod, rows, shape)
+    return res
+
+
+##############################################################
+
+def plot_dists(val_dict, color="C0", xlabel=None, stat="count", use_kde=True):
+    import seaborn as sns
+    
+    columns = len(val_dict)
+    fig, ax = plt.subplots(1, columns, figsize=(columns*3, 2.5))
+    fig_index = 0
+    for key in sorted(val_dict.keys()):
+        key_ax = ax[fig_index%columns]
+        sns.histplot(val_dict[key], ax=key_ax, color=color, bins=50, stat=stat,
+                     kde=use_kde and ((val_dict[key].max()-val_dict[key].min())>1e-8)) # Only plot kde if there is variance
+        key_ax.set_title(f"{key} " + (r"(%i $\to$ %i)" % (val_dict[key].shape[1], val_dict[key].shape[0]) if len(val_dict[key].shape)>1 else ""))
+        if xlabel is not None:
+            key_ax.set_xlabel(xlabel)
+        fig_index += 1
+    fig.subplots_adjust(wspace=0.4)
+    return fig
+
+
+
+def visualize_gradients(grads, params, color="C0", print_variance=False):
+    """
+    Inputs:
+        net - Object of class BaseNetwork
+        color - Color in which we want to visualize the histogram (for easier separation of activation functions)
+    """
+    # We limit our visualization to the weight parameters and exclude the bias to reduce the number of plots
+    grads = jax.tree_util.tree_leaves(grads)
+    grads = [g.reshape(-1) for g in grads if len(g.shape) > 1]
+    grads = {f'Layer {layer_idx*2}': g for layer_idx, g in enumerate(grads)}
+
+    ## Plotting
+    fig = plot_dists(grads, color=color, xlabel="Grad magnitude")
+    fig.suptitle("Gradient distribution", fontsize=14, y=1.05)
+    plt.show()
+    plt.close()
+
+    if print_variance:
+        for key in sorted(grads.keys()):
+            print(f"{key} - Variance: {np.var(grads[key])}")
+
+
